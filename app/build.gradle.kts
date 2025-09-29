1b6c48cf5f25130add8bf7e376e67094ccc45f45
ErickMoreira13 <marianoerick345@gmail.com>
2025-09-28 23:56:19 -0300

    Ajustes visuais

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+
 }
 
 android {
@@ -43,4 +44,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation(libs.glide)
+    annotationProcessor(libs.glideCompiler)
 }
